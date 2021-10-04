3cce20f13c8d4e671983db007b1c618057c72cec
pedro <pedropisanirodrigues@gmail.com>
2021-10-04 20:29:32 -0300

    2 commit

---
+++ listaDeTarefas.js
@@ -0,0 +1,8 @@
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', () =>{
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+    console.log(valor)
+
+})
