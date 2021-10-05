8a764e635e2f33f936ce3fcd2ebf8edc6b422c4b
pedro <pedropisanirodrigues@gmail.com>
2021-10-05 17:43:38 -0300

    Adicionado sistema de adição de tarefas

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,8 +1,25 @@
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', () =>{
+const criarTarefa = (evento) => {
+
+    evento.preventDefault()
+
+    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
     console.log(valor)
 
-})
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    lista.appendChild(tarefa)
+    const conteudo = `<p class="content">${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+}
+
+novaTarefa.addEventListener('click', criarTarefa)
+
+
+
+
